13a0502226e4467a87136978c862b4245eef7183
Anvar <ozbekdev@gmail.com>
2025-01-31 10:50:23 +0500

    Initial commit, category entity created

---
+++ src/Entity/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $categoryName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryName(): ?string
+    {
+        return $this->categoryName;
+    }
+
+    public function setCategoryName(string $categoryName): static
+    {
+        $this->categoryName = $categoryName;
+
+        return $this;
+    }
+}
