a24efa2a31674b975d251d07a2a04c8a09e3adbe
Anvar <ozbekdev@gmail.com>
2025-02-03 11:25:17 +0500

    Book entity: updated, normalization & denormalization, api filter added

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,13 +2,38 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\BookRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Delete(),
+    ],
+
+    normalizationContext: ['groups' => ['book:read']],
+    denormalizationContext: ['groups' => ['book:write']],
+)]
+
+#[ApiFilter(SearchFilter::class, properties: [
+    'id' => 'exact',
+    'name' => 'partial',
+    'author' => 'partial',
+])]
+
 class Book
 {
     #[ORM\Id]
@@ -17,16 +42,24 @@ class Book
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['book:read', 'book:write'])]
+    #[Assert\NotBlank(message: "Kitob nomini bo'sh qoldirmang")]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['book:read', 'book:write'])]
+    #[Assert\NotBlank(message: "Kitob muallifini kiritish shart")]
     private ?string $author = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['book:read', 'book:write'])]
+    #[Assert\NotBlank(message: "Kitobga kichik izoh yozish shart")]
     private ?string $description = null;
 
     #[ORM\ManyToOne(inversedBy: 'books')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['book:read', 'book:write'])]
+    #[Assert\NotBlank(message: "Kitobga kategoriya tanlang")]
     private ?Category $category = null;
 
     public function getId(): ?int
